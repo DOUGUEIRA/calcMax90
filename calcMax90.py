c6e8bf6282f078e4651d8d7ca0b63157d101cf37
marco <marco@DESKTOP-DOUGLAS>
2021-08-26 00:45:47 -0300

    Adicionar arquivos de projeto.

---
+++ calcMax90.py
@@ -0,0 +1,9 @@
+#Marcos Douglas - 08/25/21 Calc Max 90
+
+print("Seja Bem Vindo ao Calc Max 90")
+print("_______________________________________")
+
+d = int(input("Digite o primeiro número da soma:"))
+b = int(input("Digite o segundo número da soma:"))
+c = (d + b)
+print("O resultado da soma é:" , c)
